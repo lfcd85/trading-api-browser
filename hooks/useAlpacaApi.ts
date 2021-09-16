a18ece2057f3ba4357a5e204565d68c4c374b8d8
Daigo Hamasaki <daigo.hamasaki.16@gmail.com>
2021-09-17 00:43:55 +0900

    Apply ESLint outside Next.js pages

--- hooks/useAlpacaApi.ts
+++ hooks/useAlpacaApi.ts
@@ -3,7 +3,7 @@ import { AlpacaClient } from '@master-chief/alpaca'
 interface UseAlpacaApi {
   alpacaApi: AlpacaClient
 }
-  
+
 export const useAlpacaApi = (): UseAlpacaApi => {
   const key = process.env.NEXT_PUBLIC_ALPACA_API_KEY_ID as string
   const secret = process.env.NEXT_PUBLIC_ALPACA_API_SECRET_KEY as string
@@ -15,7 +15,7 @@ export const useAlpacaApi = (): UseAlpacaApi => {
       secret,
       paper,
     },
-    rate_limit: true
+    rate_limit: true,
   })
 
   return { alpacaApi }
--- utils/index.ts
+++ utils/index.ts
@@ -1,15 +1,16 @@
 export const convertCamelCaseToCapitalizedCase = (text: string): string => {
-  const result = text.replace(/([A-Z])/g, " $1")
+  const result = text.replace(/([A-Z])/g, ' $1')
   return (result.charAt(0).toUpperCase() + result.slice(1))
     .split(' ')
-    .map(word => upperCaseWords.includes(word) ? word.toUpperCase() : word)
+    .map((word) => (upperCaseWords.includes(word) ? word.toUpperCase() : word))
     .join(' ')
 }
 
 export const convertSnakeCaseToCapitalizedCase = (text: string): string =>
-  text.split('_')
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-    .map(word => upperCaseWords.includes(word) ? word.toUpperCase() : word)
+  text
+    .split('_')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .map((word) => (upperCaseWords.includes(word) ? word.toUpperCase() : word))
     .join(' ')
 
 export const upperCaseWords = ['Id']
