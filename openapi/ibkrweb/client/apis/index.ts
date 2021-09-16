a6d9c1abfa72072b7022f94384e3dd8b3e3f6db0
Daigo Hamasaki <daigo.hamasaki.16@gmail.com>
2021-09-16 14:58:04 +0900

    Run: yarn openapi:ibkrweb

---
+++ openapi/ibkrweb/client/apis/index.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from "./AccountApi";
+export * from "./AlertApi";
+export * from "./CCPBetaApi";
+export * from "./ContractApi";
+export * from "./FYIApi";
+export * from "./IBCustApi";
+export * from "./MarketDataApi";
+export * from "./OrderApi";
+export * from "./PnLApi";
+export * from "./PortfolioApi";
+export * from "./PortfolioAnalystApi";
+export * from "./ScannerApi";
+export * from "./SessionApi";
+export * from "./StreamingApi";
+export * from "./TradesApi";
