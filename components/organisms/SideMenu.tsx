5e646af80c18247d1ee350d8ad1b33418784b1ce
Daigo Hamasaki <daigo.hamasaki.16@gmail.com>
2021-09-16 16:52:21 +0900

    Add Collapse to SideMenu

--- components/organisms/SideMenu.tsx
+++ components/organisms/SideMenu.tsx
@@ -1,16 +1,52 @@
 import React from 'react'
-import { Paper, List, ListItem, Button } from '@material-ui/core'
+import {
+  Paper,
+  List,
+  ListItem,
+  ListItemText,
+  Collapse,
+} from '@material-ui/core'
+import { Broker } from '~/types'
 
 export const SideMenu: React.FC = () => {
+  const [open, setOpen] = React.useState<{
+    [broker in Broker]?: boolean
+  }>({})
+
+  const handleClick = (broker: Broker) => {
+    setOpen({ ...open, [broker]: !open[broker] })
+  }
+
   return (
     <Paper elevation={0} className="w-60 h-full border-r fixed">
       <List>
-        <ListItem>
-          <Button>Interactive Brokers</Button>
+        <ListItem button onClick={() => handleClick('interactiveBrokers')}>
+          <ListItemText>Interactive Brokers</ListItemText>
         </ListItem>
-        <ListItem>
-          <Button>Alpaca</Button>
+        <Collapse in={open.interactiveBrokers}>
+          <List disablePadding>
+            <ListItem button className="pl-8">
+              Account
+            </ListItem>
+            <ListItem button className="pl-8">
+              Market
+            </ListItem>
+          </List>
+        </Collapse>
+
+        <ListItem button onClick={() => handleClick('alpaca')}>
+          <ListItemText>Alpaca</ListItemText>
         </ListItem>
+        <Collapse in={open.alpaca}>
+          <List disablePadding>
+            <ListItem button className="pl-8">
+              Account
+            </ListItem>
+            <ListItem button className="pl-8">
+              Market
+            </ListItem>
+          </List>
+        </Collapse>
       </List>
     </Paper>
   )
---
+++ types/index.ts
@@ -0,0 +1,1 @@
+export type Broker = 'interactiveBrokers' | 'alpaca'
