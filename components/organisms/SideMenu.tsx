3ed72488c63f82ba25d5dcb35e6280f1ae15bb65
Daigo Hamasaki <daigo.hamasaki.16@gmail.com>
2021-09-16 16:24:27 +0900

    Apply styles to SideMenu

--- components/organisms/SideMenu.tsx
+++ components/organisms/SideMenu.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import { List, ListItem, Button } from '@material-ui/core'
+import { Paper, List, ListItem, Button } from '@material-ui/core'
 
 export const SideMenu: React.FC = () => {
   return (
-    <List>
-      <ListItem>
-        <Button>Interactive Brokers</Button>
-      </ListItem>
-      <ListItem>
-        <Button>Alpaca</Button>
-      </ListItem>
-    </List>
+    <Paper elevation={0} className="w-60 h-full border-r fixed">
+      <List>
+        <ListItem>
+          <Button>Interactive Brokers</Button>
+        </ListItem>
+        <ListItem>
+          <Button>Alpaca</Button>
+        </ListItem>
+      </List>
+    </Paper>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import '../styles/globals.css'
-import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
 import { SideMenu } from '~/components/organisms/SideMenu'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <SideMenu />
-      <Component {...pageProps} />
-    </>
+    <div className="flex">
+      <div className="w60">
+        <SideMenu />
+      </div>
+      <div className="ml-60">
+        <Component {...pageProps} />
+      </div>
+    </div>
   )
 }
 export default MyApp
