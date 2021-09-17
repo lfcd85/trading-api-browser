d9f73099d355a0e8fa5b8631df9ad24af8775dec
Daigo Hamasaki <daigo.hamasaki.16@gmail.com>
2021-09-17 13:16:46 +0900

    Add search window to Alpha Vantage overview page

--- pages/alphavantage/overview.tsx
+++ pages/alphavantage/overview.tsx
@@ -2,23 +2,31 @@ import React from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useAlphaVantageApi } from '~/hooks/useAlphaVantageApi'
-import { convertToCapitalCase, sleep } from '~/utils'
-import { Table, TableBody, TableCell, TableRow } from '@material-ui/core'
+import { convertToCapitalCase } from '~/utils'
+import {
+  Paper,
+  InputBase,
+  IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+} from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search'
 
 const AlphaVantageOverview: NextPage = () => {
   const { fetchAlphaVantageApi } = useAlphaVantageApi()
+  const [symbol, setSymbol] = React.useState<string>()
   const [overview, setOverview] = React.useState<object>()
 
-  React.useEffect(() => {
-    if (!overview) {
-      fetchAlphaVantageApi({
-        function: 'OVERVIEW',
-        symbol: 'MRNA', // TODO: add search area
-      })
-        .then((response) => response.json())
-        .then((data) => setOverview(data))
-    }
-  }, [fetchAlphaVantageApi])
+  const onSearch = () => {
+    fetchAlphaVantageApi({
+      function: 'OVERVIEW',
+      symbol,
+    })
+      .then((response) => response.json())
+      .then((data) => setOverview(data))
+  }
 
   return (
     <>
@@ -28,6 +36,23 @@ const AlphaVantageOverview: NextPage = () => {
 
       <main className="p-16 h-full">
         <div className="w-full h-full">
+          <Paper variant="outlined" className="w-80 flex justify-between mb-8">
+            <InputBase
+              placeholder="Search Symbol"
+              inputProps={{ 'aria-label': 'search symbol' }}
+              className="flex-grow pl-4"
+              onChange={(e) => setSymbol(e.target.value)}
+              onKeyPress={(e) => {
+                if (e.key === 'Enter') {
+                  onSearch()
+                }
+              }}
+            />
+            <IconButton aria-label="search" onClick={() => onSearch()}>
+              <SearchIcon />
+            </IconButton>
+          </Paper>
+
           {overview && (
             <Table>
               <TableBody>
